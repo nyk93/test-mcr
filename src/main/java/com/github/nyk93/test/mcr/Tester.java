dee175a22a3185a80d6c895bb241eec226285d6a
구나율 <nayulkoo@mayfarm.co.kr>
2023-06-26 16:06:38 +0900

    initial commit

---
+++ src/main/java/com/github/nyk93/test/mcr/Tester.java
@@ -0,0 +1,24 @@
+package com.github.nyk93.test.mcr;
+
+/**
+ * Maven Central Repository Tester.
+ *
+ * @author nyk93 (https://github.com/nyk93)
+ * @version 1.0
+ * @since 2023-06-26
+ */
+public class Tester {
+
+    /**
+     * greeting message.
+     */
+    private static final String MESSAGE = "Hello, World!";
+
+    /**
+     * print greeting message.
+     */
+    public void greeting() {
+        System.out.println(MESSAGE);
+    }
+
+}
